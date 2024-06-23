07b4014ccc239fffb4fc66cd4b7d2ab771027f0f
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-06-24 01:03:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from pyrogram.types import Message
 api = 'https://api.classplusapp.com/v2'
 
 # Initialize the bot with your API credentials
-api_id = 'your_api_id'
-api_hash = 'your_api_hash'
-bot_token = 'your_bot_token'
+api_id = '27862677'
+api_hash = 'e343ce2c81b2b6c2c0d6bee58284e3bd'
+bot_token = '6891484332:AAHAiVZDQZc7CHW8SRYg_iVe-rC3e20_E2w'
 
 bot = Client(
     "my_bot",
